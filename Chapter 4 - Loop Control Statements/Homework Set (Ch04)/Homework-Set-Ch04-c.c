7bd1526885dacf04a43926916abbfab1cfa82916
sakib-ar <sakibrarfile@gmail.com>
2025-06-02 03:02:02 +0600

    Slight Edit in comments on Homework 4(c)

--- Chapter 4 - Loop Control Statements/Homework Set (Ch04)/Homework-Set-Ch04-c.c
+++ Chapter 4 - Loop Control Statements/Homework Set (Ch04)/Homework-Set-Ch04-c.c
@@ -3,7 +3,7 @@
 /*  Solution:
 
     A prime number is a natural number greater than 1 that
-        has no divisors other than 1 and itself.
+        has no divisors other than 1 and itself
 
 */
 
