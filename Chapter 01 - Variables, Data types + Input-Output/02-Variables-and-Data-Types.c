b0b5fbd3e6c513bb6a4868b360473be9fba3db68
Sakib Abrar <sakibrarfile@gmail.com>
2025-06-03 23:45:58 +0600

    Delete Chapter 01 - Variables, Data types + Input-Output directory

--- Chapter 01 - Variables, Data types + Input-Output/02-Variables-and-Data-Types.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    int age = 22; // Integer data type
-    float pi = 3.14; // Decimal-point (Real number) data type
-    char percentage = '%'; // Character data type
-    
-    return 0;
-}
